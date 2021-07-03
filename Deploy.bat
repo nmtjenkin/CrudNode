95cff7b555a5a8fda1ea0a14c6c153fd13d7c567
nmtjenkin <ninad_tamore@yahoo.co.in>
2021-07-03 22:25:18 +0530

    Update4 Deploy.bat

--- Deploy.bat
+++ Deploy.bat
@@ -1,5 +1,8 @@
+ECHO "Extracting Artifact File in IIS"
 tar -xf NodeServerArtifact.tar  -C C:\inetpub\wwwroot\NodeServer
 cd C:\inetpub\wwwroot\NodeServer
 C:
+ECHO "Stopping existing Node Instance"
 taskkill /f /im node.exe
+ECHO "Starting Node App Instance"
 pm2 start Index.js
