08777d7601dca70b20ed5846d0d8d5de7fb12919
nmtjenkin <ninad_tamore@yahoo.co.in>
2021-07-03 20:32:41 +0530

    Update2 Deploy.bat

--- Deploy.bat
+++ Deploy.bat
@@ -1,5 +1,7 @@
 tar -xf NodeServerArtifact.tar  -C H:\RemoteBuilds\NodeAppBuild
 cd H:\RemoteBuilds\NodeAppBuild
 H:
+copy /Y * C:\inetpub\wwwroot\NodeServer
+C:
 taskkill /f /im node.exe
 pm2 start Index.js
